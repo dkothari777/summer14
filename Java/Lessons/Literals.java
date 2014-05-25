3464f042456c4c55232733da5814c306f47e5d6c
Shawn Kothari <dkothari777@gmail.com>
2014-05-25 14:40:40 -0700

    Commented the Interface...almost done. May need to include more methods

---
+++ Java/Lessons/Literals.java
@@ -0,0 +1,47 @@
+/** The purpose of this class is to see how literals work in the java environment and
+ * utilize it different capabilities to improve coding skils.
+ *
+ * **/
+public class Literals {
+
+    public static void main(String[] args){
+        System.out.println(0xFFFF);
+        System.out.println(0_345_345);
+        System.out.println(0B1111_1111_1101);
+        int[][] arr = new int[4][4];
+        for(int i = 0; i<arr.length; i++)
+            for(int j = 0; j<arr[i].length; j++)
+                arr[i][j]= (i!=0 ? arr[i-1][arr[i].length-1] +j : j) + (i>1 ? 1:i);
+        for(int i=0; i<arr.length; i++)
+            for(int j=0; j<arr[i].length; j++)
+                System.out.print(arr[i][j]+(j%arr[i].length==arr[i].length-1 ? "\n" : " "));
+
+
+    }
+
+    private static void decimals(){
+
+
+    }
+
+    private static void hexadecimals(){
+
+
+    }
+
+    private static void octals(){
+
+    }
+
+    private static void binary(){
+
+    }
+
+    private static void booleans(){
+
+    }
+
+    private static void strings(){
+
+    }
+}
--- Java/Projects/ISudokuSolver.java
+++ Java/Projects/ISudokuSolver.java
@@ -1,9 +1,87 @@
 public interface ISudokuSolver {
 
+    /* This method is to go through the column that the square is in
+     * and compute the numbers needed to fill that square for the column.
+     *
+     * ex.
+     * -----------------------------------------
+     * || 0 | . | 3 || X | X | X || X | X | X ||
+     * || . | 4 | . || X | X | X || X | X | X ||
+     * || . | 1 | . || X | X | X || X | X | X ||
+     * ||---------------------------------------
+     * || X | X | X || X | X | X || X | X | X ||
+     * || X | X | X || X | X | X || X | X | X ||
+     * || X | X | X || X | X | X || X | X | X ||
+     * ||---------------------------------------
+     * || X | X | X || X | X | X || X | X | X ||
+     * || X | X | X || X | X | X || X | X | X ||
+     * || X | X | X || X | X | X || X | X | X ||
+     * -----------------------------------------
+     *
+     * Key:
+     * '.' = blank square
+     * 'X' = pseudo values
+     *
+     *  */
+    private int[] checkColumnPossible(int[][] board, int row, int col);
 
-    public int[] checkColumnPossible(int[][] board, int row, int col);
-    public int[] checkRowPossible(int[][] board, int row, int col);
-    public int[] checkSquarePossible(int[][] board, int row, int col);
-    public int[] inclusion(int[] c, int[] r; int[] s);
+    /* This method is to go through the row that the square is in and
+     * compute the numbers needed to fill that square for the row.
+     *
+     * ex.
+     * -----------------------------------------
+     * || X | . | X || X | X | X || X | X | X ||
+     * || X | 4 | X || X | X | X || X | X | X ||
+     * || X | 1 | X || X | X | X || X | X | X ||
+     * ||---------------------------------------
+     * || X | 7 | X || X | X | X || X | X | X ||
+     * || X | . | X || X | X | X || X | X | X ||
+     * || X | 9 | X || X | X | X || X | X | X ||
+     * ||---------------------------------------
+     * || X | . | X || X | X | X || X | X | X ||
+     * || X | . | X || X | X | X || X | X | X ||
+     * || X | 0 | X || X | X | X || X | X | X ||
+     * -----------------------------------------
+     *
+     * Key:
+     * '.' = blank square
+     * 'X' = pseudo values
+     *
+     *
+     * */
+    private int[] checkRowPossible(int[][] board, int row, int col);
+
+    /* This method is to go through the 3x3 square that the square is in
+     * and compute the numbers needed to fill that blank square for the
+     * 3x3 square. (Complex I know)
+     *
+     * ex.
+     * -----------------------------------------
+     * || 0 | . | 3 || X | X | X || X | X | X ||
+     * || . | 4 | . || X | X | X || X | X | X ||
+     * || . | 1 | . || X | X | X || X | X | X ||
+     * ||---------------------------------------
+     * || X | X | X || X | X | X || X | X | X ||
+     * || X | X | X || X | X | X || X | X | X ||
+     * || X | X | X || X | X | X || X | X | X ||
+     * ||---------------------------------------
+     * || X | X | X || X | X | X || X | X | X ||
+     * || X | X | X || X | X | X || X | X | X ||
+     * || X | X | X || X | X | X || X | X | X ||
+     * -----------------------------------------
+     *
+     * Key:
+     * '.' = blank square
+     * 'X' = pseudo values
+     *
+     *  Lets take the first square. It contains the 0, 1, 3, and 4. This means that this
+     *  method should return 2,5,6,7,8, and 9 in an array.
+     *
+     *  Method Call: checkSquarePossible(board, 1, 1);
+     *      returns an array; {2, 5, 6, 7, 8, 9};
+     * */
+    private int[] checkSquarePossible(int[][] board, int row, int col);
+
+    private int[] inclusion(int[] c, int[] r; int[] s);
 
 }
